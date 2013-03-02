de2df58037011a3937fba2bb562341d5168de982
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2013-03-02 23:03:02 +0200

    Added list of people to main page.

--- app/scripts/views/index.js
+++ app/scripts/views/index.js
@@ -3,17 +3,35 @@
 define([
     'backbone',
     // templates
-    'text!templates/index.hbs'
+    'text!templates/index.hbs',
+    // collections
+    'collections/people',
+    // views
+    'views/indexPerson'
 
-], function(Backbone, indexTemplate) {
+], function(Backbone, indexTemplate, PeopleCollection, PersonView) {
     'use strict';
 
     var IndexView = Backbone.View.extend({
         el: '#content',
 
+        initialize: function() {
+            this.people = new PeopleCollection();
+            this.listenTo(this.people, 'add', this.addPerson);
+            this.people.fetch({ update: true });
+        },
+
         render: function() {
             this.$el.html(indexTemplate);
+
+            // vars from template
+            this.$people = this.$('ul');
             return this;
+        },
+
+        addPerson: function(model) {
+            var personView = new PersonView({ model: model });
+            this.$people.append(personView.render().el);
         }
     });
 
---
+++ app/scripts/views/indexPerson.js
@@ -0,0 +1,26 @@
+/*global define */
+
+define([
+    'backbone',
+    'handlebars',
+
+    // templates
+    'text!templates/indexPerson.hbs'
+], function(Backbone, Handlebars, personTemplate) {
+    'use strict';
+
+    var PersonView = Backbone.View.extend({
+
+        tagName: 'li',
+
+        template: Handlebars.compile(personTemplate),
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+
+            return this;
+        },
+    });
+
+    return PersonView;
+});
